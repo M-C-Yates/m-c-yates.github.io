eae1b9303e149ea512fb729ade8df550817cc5c0
VirtualSalesman <Mat.C.Yates@gmail.com>
2019-06-11 12:06:03 -0400

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
 import Layout from "../components/layout"
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <main className="home">
-        <div className="home__info">
-          <h1 className="home__info__title">Hello!</h1>
-          <h2 className="home__info__subtitle">
-            I'm Matthew a fullstack developer in Lynchburg, Va
-          </h2>
-          <p className="home__info__text">
-            Need a developer? <Link to="/contact">Contact me.</Link>
-          </p>
-        </div>
-      </main>
-    </Layout>
+    <React.Fragment>
+      <Layout>
+        <Helmet>
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+        </Helmet>
+        <main className="home">
+          <div className="home__info">
+            <h1 className="home__info__title">Hello!</h1>
+            <h2 className="home__info__subtitle">
+              I'm Matthew a fullstack developer in Lynchburg, Va
+            </h2>
+            <p className="home__info__text">
+              Need a developer? <Link to="/contact">Contact me.</Link>
+            </p>
+          </div>
+        </main>
+      </Layout>
+      <script>
+        if (window.netlifyIdentity){" "}
+        {window.netlifyIdentity.on("init", user => {
+          if (!user) {
+            window.netlifyIdentity.on("login", () => {
+              document.location.href = "/admin/"
+            })
+          }
+        })}
+      </script>
+    </React.Fragment>
   )
 }
 
--- src/pages/post.js
+++ src/pages/post.js
@@ -1,33 +1,34 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
-    }
-  }
-`
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 
 export const Post = props => {
-  const post = props.data.markdownRemark
+  // const post = props.data.markdownRemark
 
   return (
     <Layout>
       <article className="post">
-        <h2 className="post__title">{post.frontmatter.title}</h2>
-        <h4 className="post__date">{post.frontmatter.date}</h4>
-        <div
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          className="post__body"
-        />
+        <h2 className="post__title" />
+        <h4 className="post__date" />
+        <div className="post__body" />
       </article>
     </Layout>
   )
 }
 
 export default Post
+
+// {post.frontmatter.date}
+// {post.frontmatter.title}
+// dangerouslySetInnerHTML={{ __html: post.html }}
