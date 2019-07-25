c67a74df7f4824726cfe4f84abca28c1ea745083
Matthew Yates <Mat.C.Yates@gmail.com>
2019-07-25 13:08:52 -0400

    updated projects

--- src/pages/index/Projects.js
+++ src/pages/index/Projects.js
@@ -27,6 +27,35 @@ export const Projects = () => {
   return (
     <section className="projects">
       <article className="projects__card">
+        <h3 className="projects__card__title">WallWander</h3>
+        <Img
+          className="projects__card__img"
+          fluid={srcs[1].fluid}
+          alt="Wallwander project Image"
+        />
+        <ul className="projects__card__stack">
+          <li>React</li>
+          <li>Redux</li>
+          <li>PostgreSQL</li>
+          <li>Node</li>
+          <li>Typescript(backend only)</li>
+        </ul>
+        <p className="projects__card__description">
+          A wallpaper sharing app inspired by https://wallhaven.cc/. A
+          React/Redux App powered by an Express api written in Typescript and
+          using Postgres.
+        </p>
+        <div className="projects__card__link">
+          <a>View Site</a>
+          <a href="https://github.com/M-C-Yates/WallWander-frontend">
+            Frontend Code
+          </a>
+          <a href="https://github.com/M-C-Yates/WallWander-backend">
+            Backend Code
+          </a>
+        </div>
+      </article>
+      <article className="projects__card">
         <h3 className="projects__card__title">BnB</h3>
         <Img
           className="projects__card__img"
@@ -46,11 +75,11 @@ export const Projects = () => {
           with all data stored in a postgres container managed by Prisma.
         </p>
         <div className="projects__card__link">
+          <a>View Site</a>
           <a href="https://github.com/M-C-Yates/bnb-frontend">Frontend Code</a>
           <a href="https://github.com/M-C-Yates/bnb-backend">Backend Code</a>
         </div>
       </article>
-      <article className="projects__card">Fullstack todoApp</article>
     </section>
   )
 }
