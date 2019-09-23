a0aa462eb0d06ad9ec3fd05033d51e0e33f66f8d
Matthew Yates <Mat.C.Yates@gmail.com>
2019-09-23 11:42:28 -0400

    Updates

---
+++ component---src-pages-index-header-info-js-38c6f288fd29cf0ef5d9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{192:function(e,t,n){"use strict";n.r(t),n.d(t,"Info",function(){return o});var a=n(0),c=n.n(a),o=function(){return c.a.createElement("div",{className:"home__info"},c.a.createElement("h2",null,"Fullstack developer"),c.a.createElement("a",{href:"https://github.com/M-C-Yates"},"Github"))};t.default=o}}]);
+//# sourceMappingURL=component---src-pages-index-header-info-js-38c6f288fd29cf0ef5d9.js.map
