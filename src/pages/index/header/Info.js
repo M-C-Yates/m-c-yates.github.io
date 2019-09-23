20bae7b0076eb0a3f17ede2d58df330237bfaa25
Matthew Yates <Mat.C.Yates@gmail.com>
2019-09-23 11:41:58 -0400

    .

--- src/pages/index/header/Info.js
+++ src/pages/index/header/Info.js
@@ -3,6 +3,7 @@ import React from "react"
 export const Info = () => (
   <div className="home__info">
     <h2>Fullstack developer</h2>
+    <a href="https://github.com/M-C-Yates">Github</a>
   </div>
 )
 
